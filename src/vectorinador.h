cbe0eeab0a7330df3cdb1bb972679e86a952552e
Pilsenshot <wwvicentemd@gmail.com>
2026-04-23 15:31:45 -0400

    Actualización de la cabecera del prototipo.
    Se corrigieron los errores de los punteros en la estructura de  las funciones.

--- src/vectorinador.h
+++ src/vectorinador.h
@@ -7,12 +7,12 @@
 typedef struct {
 	float x;
 	float y;
-} Vector;
+} Vector;//corregi el id porque no es necesario, dentro del archivo de funciones hay que definir los valores
 
-Vector suma_vector(Vector a, Vector b);
-Vector resta_vector(Vector a, Vector b);
-float producto_punto(Vector a, Vector b);
-float modulo_vector(Vector v);
+Vector suma_vector(Vector* a, Vector* b);
+Vector resta_vector(Vector* a, Vector* b);
+float producto_punto(Vector* a, Vector* b);
+float modulo_vector(Vector* v);
 void imprimir_v(Vector v,const char*nombre); //Se define el vector "v", con un punto se puede ingresar a su valor (v.x por ejemplo)
 // el const char*nombre es un puntero con constante, traduce la funcion para que la podamos ver en la consola PERO no la modifica
 
