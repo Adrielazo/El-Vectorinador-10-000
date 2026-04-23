94ff1d647a1baa3143424af0f6032fcbf8b999b1
lukakas16 <lukilokin@gmail.com>
2026-04-23 13:36:30 -0400

    Cuerpo del programa
    Se definen los vectores nomas

---
+++ src/vectorinador.c
@@ -0,0 +1,23 @@
+#include "vectorinador.h"
+
+Vector suma_vector(Vector a, Vector b){
+	Vector resultado = {a.x + b.x, a.y + b.y};
+	return resultado;
+}//¿Por que no poner los printf acà? Mas que nada por optimizaciòn, lo que estamos buscando en este archivo es dejar definidas las
+//funciones matematicas del calculo de vectores, meter print puede ser un gasto de lineas y memoria innecesarios en este
+//archivo, por lo que se lo dejamos al archivo main el cual se encargarà de mostrar todos los print en la consola para que
+//el usuario pueda interactuar
+
+
+Vector resta_vector(Vector a, Vector b){
+	Vector resultado = {a.x - b.x, a.y - b.y};
+	return resultado;
+}
+
+float producto_punto(Vector a, Vector b){
+	return float {a.x*b.x + a.y*b.y}; 
+}
+
+float modulo_vector(Vector v){
+	return float{sqrt((v.x*v.x)+(v.y*v.y))};
+}
