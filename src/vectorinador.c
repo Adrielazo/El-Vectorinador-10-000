9fd7c5291b120e6b0ee5f3087341a97af2410a4c
kourzed <76547566+kourzed@users.noreply.github.com>
2026-04-23 13:41:46 -0400

    Corrección ortografía 2

--- src/vectorinador.c
+++ src/vectorinador.c
@@ -3,7 +3,7 @@
 Vector suma_vector(Vector a, Vector b){
 	Vector resultado = {a.x + b.x, a.y + b.y};
 	return resultado;
-}//¿Por que no poner los printf acá? Mas que nada por optimización, lo que estamos buscando en este archivo es dejar definidas las
+}//¿Por que no poner los printf acá? Más que nada por optimización, lo que estamos buscando en este archivo es dejar definidas las
 //funciones matemáticas del cálculo de vectores, meter print puede ser un gasto de líneas y memoria innecesarios en este
 //archivo, por lo que se lo dejamos al archivo main, el cual se encargará de mostrar todos los print en la consola para que
 //el usuario pueda interactuar
