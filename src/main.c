8e8d1fa24ce55eade2dddc5dd82f5a0ed6c0574f
lukakas16 <lukilokin@gmail.com>
2026-04-24 12:31:04 -0400

    menú hecho

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 int main(){
 	Vector *v1=(Vector*)malloc(sizeof(Vector));
 	Vector *v2=(Vector*)malloc(sizeof(Vector));
+	int opcion;
 
 	printf("Ingrese datos del primer vector: \n");
 	printf("Valor en x: ");
@@ -14,17 +15,33 @@ int main(){
 	printf("\nValor en y: ");
 	scanf("%f", &v2->y);
 	
-	Vector s = suma_vector(v1,v2);
-	printf("\nSuma: (%.2f,%.2f)",s.x, s.y);
-
-	Vector r = resta_vector(v1,v2);
-	printf("\nResta: (%.2f,%.2f)",r.x, r.y);
-
-	float pp = producto_punto(v1,v2);
-	printf("\nProducto punto: %2f", pp);
-
-	float mod= modulo_vector(v1);
-	printf("\nMódulo: %2f \n", mod);
+	printf("\n===MENÚ===\n¿Qué te gustaría hacer?");
+	printf("\n1.- Sumar vectores.");
+	printf("\n2.- Restar vectores.");
+	printf("\n3.- Producto punto.");
+	printf("\n4.- Calcular módulo.\n");
+	printf()
+	scanf("%d",opcion);
+	switch opcion{
+		case 1:
+		Vector s = suma_vector(v1,v2);
+		printf("\nSuma: (%.2f,%.2f)",s.x, s.y);
+		case 2:
+		Vector r = resta_vector(v1,v2);
+		printf("\nResta: (%.2f,%.2f)",r.x, r.y);
+		case 3:
+		float pp = producto_punto(v1,v2);
+		printf("\nProducto punto: %2f", pp);
+		case 4:
+		float mod1= modulo_vector(v1);
+		printf("\nMódulo vector 1: %2f \n", mod1);
+		float mod2= modulo_vector(v2);
+		printf("\nMódulo vector 2: %2f \n", mod2);
+		case 5:
+		printf("WIP");
+		default:
+		printf("opción inválida.");
+	}
 
 
 	free(v1);
