2ac38df0fd1f8eadaae2b3eab4bdc46cffd7baf0
Pilsenshot <wwvicentemd@gmail.com>
2026-05-28 22:44:55 -0400

    Actualización
    Corrección en la lógica de ejecución con la clase Vector ya implementada.

--- src/main.cpp
+++ src/main.cpp
@@ -1,127 +1,188 @@
 #include "vectorinador10000.hpp"
 
-int main() {
-    std::cout << "Bienvenido!" << std::endl;
-    int opcion;
-    int operacion;
-    while (true) {
-        std::cout << "¿Qué te gustaría hacer?" << std::endl;
-        std::cout << "1. Agregar un nuevo vector" << std::endl;
-        std::cout << "2. Modificar un vector existente" << std::endl;
-        std::cout << "3. Mostrar todos los vectores almacenados" << std::endl;
-        std::cout << "4. Eliminar un vector" << std::endl;
-        std::cout << "5. Operar vectores" << std:: endl;
-        std::cout << "6. Salir" << std::endl;
-        std::cout << "Ingrese el número de la opción deseada: ";
-        std::cin >> opcion;
-        std::cout << std::endl;
+void mostrarVector(const std::vector<Vector>& lista){
+    std::cout << "\n---Vectores en memoria---\n";
+    if (lista.empty()){
+        std::cout << "No hay vectores en memoria.";
+        return;
+    }
+    for (size_t i = 0; i < lista.size(); ++i) {
+        std::cout<<"Indice["<<i<<"]: ";
+        lista[i].imprimir_vector();
+    }
+}
 
-        switch (opcion) {
+int main (){
+    std::vector<Vector> listaVectores;
+    int opcion = 0;
+    int operacion = 0;
+    do{
+        std::cout<<"\n---Menu de opciones---\n";
+        std::cout<<"1. Crear un nuevo vector\n";
+        std::cout<<"2. Ver todos los vectores\n";
+        std::cout<<"3. Modificar un vector existente\n";
+        std::cout<<"4. Operar con vectores\n";
+        std::cout<<"5. Salir\n";
+        std::cout<<"Seleccione una opcion: ";
+        std::cin>>opcion;
+
+        switch (opcion){
             case 1: {
-                nuevo_vector(lista_vec, cantidad_vec);
+                float x, y, z;
+                std::cout<<"Ingrese las componentes del vector (X Y Z):\n ";
+                std::cout<<"Ingrese la componente en X: "; std::cin>>x;
+                std::cout<<"Ingrese la componente en Y: "; std::cin>>y;
+                std::cout<<"Ingrese la componente en Z: "; std::cin>>z;
+                listaVectores.push_back(Vector(x, y, z));
+                std::cout<<"¡Vector creado exitosamente!.\n";
                 break;
             }
             case 2: {
-                int indice_mod;
-                std::cout << "Ingrese el número del vector a modificar: ";
-                std::cin >> indice_mod;
-                modificar_vector(lista_vec, cantidad_vec, indice_mod-1);
+                mostrarVector(listaVectores);
+                break;  
+            } 
+            case 3:{
+                if (listaVectores.empty()){
+                    std::cout<<"No hay vectores para modificar.\n";
+                    break;
+                }
+                size_t indice;
+                std::cout<<"Ingrese el indice del vector a modificar: ";
+                std::cin>>indice;
+
+                if (indice < listaVectores.size()){
+                    float X, Y, Z;
+                    std::cout << "Ingrese los nuevos componentes X,Y,Z (Separados por espacio)";
+                    std::cin >> X >> Y >> Z;
+                    listaVectores[indice].modificar_vector(X, Y, Z);
+                    std::cout<<"¡Vector modificado exitosamente!.\n";   
+                } else{
+                    std::cout<<"Indice invalido. No se modifico ningun vector.\n";
+                }
                 break;
             }
-            case 3: {
-                mostrar_vectores(lista_vec, cantidad_vec);
-                break;
-            }
-            case 4: {
-                int indice_elim;
-                std::cout << "Ingrese el número del vector a eliminar: ";
-                std::cin >> indice_elim;
-                eliminar_vector(lista_vec, cantidad_vec, indice_elim-1);
-                break;
-            }
-            case 5: {
-                std::cout << "¿Qué operación deseas realizar?" << std::endl;
-                std::cout << "1. Suma de vectores" << std::endl;
-                std::cout << "2. Resta de vectores" << std::endl;
-                std::cout << "3. Producto punto" << std::endl;
-                std::cout << "4. Producto cruz" << std::endl;
-                std::cout << "5. Módulo de un vector" << std::endl;
-                std::cout << "6. Volver al menú principal" << std::endl;
-                std::cout << "Ingrese el número de la operación deseada: ";
+            
+            case 4:{
+                std::cout << "¿Que operacion deseas realizar?\n";
+                std::cout << "1. Suma de vectores\n" ;
+                std::cout << "2. Resta de vectores\n" ;
+                std::cout << "3. Producto punto\n" ;
+                std::cout << "4. Producto cruz\n" ;
+                std::cout << "5. Modulo de un vector\n" ;
+                std::cout << "6. Volver al menu principal\n" ;
+                std::cout << "Ingrese el numero de la operacion deseada: ";
                 std::cin >> operacion;
+
                 switch (operacion) {
                     case 1: {
-                        int indice_a, indice_b;
-                        mostrar_vectores(lista_vec, cantidad_vec);
-                        std::cout << "Ingrese el número del primer vector: ";
-                        std::cin >> indice_a;
-                        std::cout << "Ingrese el número del segundo vector: ";
-                        std::cin >> indice_b;
-                        std::vector<float> resultado_suma = suma_v(indice_a-1, indice_b-1, lista_vec);
-                        std::cout << "Resultado de la suma: {" << resultado_suma[0] << " , " << resultado_suma[1] << " , " << resultado_suma[2] << "}" << std::endl;
-                        break;
-                    }
-                    case 2: {
-                        int indice_a, indice_b;
-                        mostrar_vectores(lista_vec, cantidad_vec);
-                        std::cout << "Ingrese el número del primer vector: ";
-                        std::cin >> indice_a;
-                        std::cout << "Ingrese el número del segundo vector: ";
-                        std::cin >> indice_b;
-                        std::vector<float> resultado_resta = resta_v(indice_a-1, indice_b-1, lista_vec);
-                        std::cout << "Resultado de la resta: {" << resultado_resta[0] << " , " << resultado_resta[1] << " , " << resultado_resta[2] << "}" << std::endl;
-                        break;
-                    }
-                    case 3: {
-                        int indice_a, indice_b;
-                        mostrar_vectores(lista_vec, cantidad_vec);
-                        std::cout << "Ingrese el número del primer vector: ";
-                        std::cin >> indice_a;
-                        std::cout << "Ingrese el número del segundo vector: ";
-                        std::cin >> indice_b;
-                        float resultado_producto_punto = producto_punto(indice_a-1, indice_b-1, lista_vec);
-                        std::cout << "Resultado del producto punto: " << resultado_producto_punto << std::endl;
-                        break;
-                    }
-                    case 4: {
-                        int indice_a, indice_b;
-                        mostrar_vectores(lista_vec, cantidad_vec);
-                        std::cout << "Ingrese el número del primer vector: ";
-                        std::cin >> indice_a;
-                        std::cout << "Ingrese el número del segundo vector: ";
-                        std::cin >> indice_b;
-                        std::vector<float> resultado_producto_cruz = producto_cruz(indice_a-1, indice_b-1, lista_vec);
-                        std::cout << "Resultado del producto cruz: {" << resultado_producto_cruz[0] << " , " << resultado_producto_cruz[1] << " , " << resultado_producto_cruz[2] << "}" << std::endl;
-                        break;
-                    }
-                    case 5: {
-                        int indice;
-                        mostrar_vectores(lista_vec, cantidad_vec);
-                        std::cout << "Ingrese el número del vector: ";
-                        std::cin >> indice;
-                        float resultado_modulo = modulo_v(indice-1, lista_vec);
-                        std::cout << "Resultado del módulo: " << resultado_modulo << std::endl;
-                        break;
-                    }
-                    case 6: {
-                        std::cout << "Volviendo..." << std::endl;
+                        if (listaVectores.size() < 2){
+                            std::cout<<"Se necesitan al menos dos vectores para realizar la suma";
+                            break;
+                        }
+                        mostrarVector(listaVectores);
+                        size_t indice1, indice2;
+                        std::cout<<"Ingrese el indice del primer vector: "; std::cin>>indice1;
+                        std::cout<<"Ingrese el indice del segundo vector: "; std::cin>>indice2;
+                        if(indice1<listaVectores.size() && indice2<listaVectores.size()){
+                            Vector resultado = listaVectores[indice1] + listaVectores[indice2];
+                            std::cout<<"Resultado de la suma: ";
+                            resultado.imprimir_vector();
+                        } else{
+                            std::cout<<"Uno o ambos indices son invalidos. No se realizo la suma.\n";
+                        }
                         break;
                     
                     }
-                
-                    default: {
-                        std::cout << "Opción no válida. Volviendo al menú principal." << std::endl;
+                    case 2:{
+                        if (listaVectores.size() < 2){
+                            std::cout<<"Se necesitan al menos dos vectores para realizar la resta";
+                            break;
+                        }
+                        mostrarVector(listaVectores);
+                        size_t indice1, indice2;
+                        std::cout<<"Ingrese el indice del primer vector: "; std::cin>>indice1;
+                        std::cout<<"Ingrese el indice del segundo vector: "; std::cin>>indice2;
+
+                        if(indice1<listaVectores.size() && indice2<listaVectores.size()){
+                            Vector resultado = listaVectores[indice1] - listaVectores[indice2];
+                            std::cout<<"Resultado de la resta: ";
+                            resultado.imprimir_vector();
+                        } else{
+                            std::cout<<"Uno o ambos indices son invalidos. No se realizo la resta.\n";
+                        }
                         break;
                     }
-                }
-                break;
-            }   
-            case 6: {
-                std::cout << "este es el caso 6" << std::endl;
-                std::cout << "¡Hasta luego!" << std::endl;
-                return 0;
+                    case 3:{
+                        if (listaVectores.size() < 2){
+                            std::cout<<"Se necesitan al menos dos vectores para calcular el producto punto";
+                            break;
+                        }
+                        mostrarVector(listaVectores);
+                        size_t indice1, indice2;
+                        std::cout<<"Ingrese el indice del primer vector: "; std::cin>>indice1;
+                        std::cout<<"Ingrese el indice del segundo vector: "; std::cin>>indice2;
+                        if(indice1<listaVectores.size() && indice2<listaVectores.size()){
+                            float punto = listaVectores[indice1].productopunto(listaVectores[indice2]);
+                            std::cout<<"Resultado del producto punto: "<<punto<<"\n";
+                        } else{
+                            std::cout<<"Uno o ambos indices son invalidos. No se realizo el calculo del producto punto.\n";
+                        }
+                        break;
+                    }
+                    case 4:{
+                        if (listaVectores.size() < 2){
+                            std::cout<<"Se necesitan al menos dos vectores para calcular el producto cruz";
+                            break;
+                        }
+                        mostrarVector(listaVectores);
+                        size_t indice1, indice2;
+                        std::cout<<"Ingrese el indice del primer vector: "; std::cin>>indice1;
+                        std::cout<<"Ingrese el indice del segundo vector: "; std::cin>>indice2;
+                        if(indice1<listaVectores.size() && indice2<listaVectores.size()){
+                            Vector resultado = listaVectores[indice1].productocruz(listaVectores[indice2]);
+                            std::cout<<"Resultado del producto cruz: ";
+                            resultado.imprimir_vector();
+                        } else{
+                            std::cout<<"Uno o ambos indices son invalidos. No se realizo el calculo del producto cruz.\n";
+                        }
+                        break;
+                    }
+                    case 5:{
+                        if (listaVectores.empty()){
+                            std::cout<<"\nError: No hay vectores almacenados.\n";
+                            break;
+                        }
+                        mostrarVector(listaVectores);
+                        size_t indice;
+                        std::cout<<"Ingrese el indice del vector para calcular su modulo: "; std::cin>>indice;
+                        if(indice<listaVectores.size()){
+                            float modulo = listaVectores[indice].modulo();
+                            std::cout<<"El modulo del vector es: "<<modulo<<"\n";
+                        } else{
+                            std::cout<<"Indice invalido.\n";
+                        }
+                        break;
+                    }
+                    case 6:{
+                        std::cout<<"Volviendo...\n";
+                        break;
+                    }
+                    default:{
+                        std::cout<<"Opcion invalida. Por favor, seleccione una opcion del 1 al 6.\n";
+                        break;
+                    }   
             }
-          
+            case 5:{
+                std::cout<<"¡Hasta luego!\n";
+                break;
+            }
+            default:{
+                std::cout<<"Opcion invalida. Por favor, seleccione una opcion del 1 al 5.\n";
+                break;
+             }
+        
+            }
         }
-    }
-}
+    } while (opcion != 5); 
+    return 0;
+}   
