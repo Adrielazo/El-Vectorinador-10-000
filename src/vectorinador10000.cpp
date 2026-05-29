b3c6792c2bcd652f9351bbcdf9aa091c5400527e
lukakas16 <lukilokin@gmail.com>
2026-05-29 15:54:25 -0400

    Implementacion de funciones

--- src/vectorinador10000.cpp
+++ src/vectorinador10000.cpp
@@ -37,3 +37,24 @@ void Vector::modificar_vector(float nuevoX, float nuevoY, float nuevoZ) {
 Vector Vector::mult_escalar(float escalar) const {
     return Vector(this->X * escalar, this->Y *escalar, this->Z * escalar);
 }
+
+Vector Vector::unitario() const {
+    float mod = this->modulo();
+    if (mod == 0) {
+        std::cerr << "Error: No se puede calcular el vector unitario de un vector nulo.\n";
+        return Vector(0, 0, 0);
+    }
+    return Vector(this->X / mod, this->Y / mod, this->Z / mod);    
+}
+
+float Vector::angulo_entre(const Vector& v) const {
+    float punto = this->productopunto(v);
+    float mod1= this->modulo();
+    float mod2 = v.modulo();
+    if (mod1 == 0 || mod2 == 0) {
+        std::cerr << "Error: No se puede calcular el ángulo entre un vector nulo.\n";
+        return 0.0;
+    }
+    float cos_angulo = punto / (mod1 * mod2);
+    return std::acos(cos_angulo)*(180.0/M_PI);
+}
