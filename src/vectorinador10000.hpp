58b28d3b2e41cf5bad0dd5f358468574a23cb50e
Pilsenshot <wwvicentemd@gmail.com>
2026-05-28 22:38:13 -0400

    Actualización
    Corrección del Header incluyendo la estructura de la clase Vector y sus métodos

--- src/vectorinador10000.hpp
+++ src/vectorinador10000.hpp
@@ -1,24 +1,23 @@
+#ifndef VECTORINADOR10000_HPP
+#define VECTORINADOR10000_HPP
 #include <iostream>
+#include <cmath>
 #include <vector>
 
+class Vector{
+    private:
+        float X,Y,Z;
+    public:
+        Vector(float X = 0.0, float Y = 0.0, float Z = 0.0 );
+        
+        Vector operator+(const Vector& v) const;
+        Vector operator-(const Vector& v) const;
+        
+        float productopunto(const Vector& v) const;
+        float modulo() const;
+        Vector productocruz(const Vector& v) const;
+        void imprimir_vector() const;
+        void modificar_vector(float nuevoX, float nuevoY, float nuevoZ);
 
-//Se definen variables:
-std::vector<std::vector<float>> lista_vec (10);
-int cantidad_vec = 0;
-
-//Se definen funciones:
-
-void nuevo_vector(std::vector<std::vector<float>>& lista_vec, int& cantidad_vec);
-
-void modificar_vector(std::vector<std::vector<float>>& lista_vec, int cantidad_vec, int indice);
-
-void mostrar_vectores(std::vector<std::vector<float>>& lista_vec, int cantidad_vec);
-
-void eliminar_vector(std::vector<std::vector<float>>& lista_vec, int& cantidad_vec, int indice);
-
-//Se definen operaciones:
-std::vector<float> suma_v(int indice_a, int indice_b, std::vector<std::vector<float>>& lista_vec);
-std::vector<float> resta_v(int indice_a, int indice_b, std::vector<std::vector<float>>& lista_vec);
-float producto_punto(int indice_a, int indice_b, std::vector<std::vector<float>>& lista_vec);
-std::vector<float> producto_cruz(int indice_a, int indice_b, std::vector<std::vector<float>>& lista_vec);
-float modulo_v(int indice, std::vector<std::vector<float>>& lista_vec);
+};
+#endif 
