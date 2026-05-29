f125b457b2c1274232d5174e8ab1b2289b6eee6e
Pilsenshot <wwvicentemd@gmail.com>
2026-05-28 22:41:16 -0400

    Actualización
    Corrección de la implementación de los métodos del Header.

--- src/vectorinador10000.cpp
+++ src/vectorinador10000.cpp
@@ -1,65 +1,35 @@
 #include "vectorinador10000.hpp"
+Vector::Vector(float X, float Y, float Z) : X(X), Y(Y), Z(Z) {}
 
-//Se implementan funciones:
-
-void nuevo_vector(std::vector<std::vector<float>>& lista_vec, int& cantidad_vec) {
-    if (cantidad_vec < lista_vec.size()) {
-        std::vector<float> vec_temp = {0, 0, 0};
-        std::cout << "Ingrese componente en X: ";
-        std::cin >> vec_temp[0];
-        std::cout << "Ingrese componente en Y: ";
-        std::cin >> vec_temp[1];
-        std::cout << "Ingrese componente en Z: ";
-        std::cin >> vec_temp[2];
-        lista_vec.push_back(vec_temp);
-        cantidad_vec++;
-        std::cout << "Vector agregado exitosamente." << std::endl;
-        return;
-    }
-    else {
-        std::cout << "No se pueden agregar más vectores. Capacidad máxima alcanzada." << std::endl;
-        return;
-    }
+Vector Vector::operator+(const Vector& v) const {
+    return Vector(this->X + v.X, this->Y + v.Y, this->Z + v.Z);
 }
 
-void modificar_vector(std::vector<std::vector<float>>& lista_vec, int cantidad_vec, int indice) {
-    if (indice < cantidad_vec && indice >=0) {
-        std::vector<float> vec_temp = {0,0,0};
-        std::cout << "Ingrese nueva componente en X: ";
-        std::cin >> vec_temp[0];
-        std::cout << "Ingrese nueva componente en Y: ";
-        std::cin >> vec_temp[1];
-        std::cout << "Ingrese nueva componente en Z: ";
-        std::cin >> vec_temp[2];
-        lista_vec[indice] = vec_temp;
-        std::cout << "Vector modificado exitosamente." << std::endl;
-    }
-    else {
-        std::cout << "Índice no válido. No se puede modificar el vector." << std::endl;
-    }  
+Vector Vector::operator-(const Vector& v) const {
+    return Vector(this->X - v.X, this->Y - v.Y, this->Z - v.Z);
 }
 
-void mostrar_vectores(std::vector<std::vector<float>>& lista_vec, int cantidad_vec) {
-    if (cantidad_vec >0) {
-        std::cout << "------VECTORES ALMACENADOS------" << std::endl;
-        for (int i = 0; i < cantidad_vec; i++) {
-            std::cout << i+1 << ".- {" << lista_vec[i][0] << " , " << lista_vec[i][1] << " , " << lista_vec[i][2] << "}" << std::endl;
-
-        }
-    
-    }
-    else {
-        std::cout << "No hay vectores almacenados." << std::endl;
-    }
+Vector Vector::productocruz(const Vector& v) const{
+    float cruzX = (this->Y * v.Z) - (this->Z * v.Y);
+    float cruzY = (this->Z * v.X) - (this->X * v.Z);
+    float cruzZ = (this->X * v.Y) - (this->Y * v.X);
+    return Vector(cruzX, cruzY, cruzZ);
 }
 
-void eliminar_vector(std::vector<std::vector<float>>& lista_vec, int& cantidad_vec, int indice) {
-    if (indice < cantidad_vec && indice >=0) {
-        lista_vec.erase(lista_vec.begin() + indice);
-        cantidad_vec--;
-        std::cout << "Vector eliminado exitosamente." << std::endl;
-    }
-    else {
-        std::cout << "Índice no válido. No se puede eliminar el vector." << std::endl;
-    }
+float Vector::productopunto(const Vector& v) const {
+    return (this->X * v.X) + (this->Y * v.Y) + (this->Z * v.Z);
+}
+
+float Vector::modulo() const {
+    return std::sqrt((X * X) + (Y * Y) + (Z * Z));
+}
+
+void Vector::imprimir_vector() const {
+    std::cout << "Vector: (" << X << ", " << Y << ", " << Z << ")" << std::endl;
+}
+
+void Vector::modificar_vector(float nuevoX, float nuevoY, float nuevoZ) {
+    X = nuevoX;
+    Y = nuevoY;
+    Z = nuevoZ;
 }
