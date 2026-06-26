f78956fa8147612cd5f14d684b23c3b844a7441d
Pilsenshot <wwvicentemd@gmail.com>
2026-06-25 21:12:11 -0400

    Modificación
    Orden y eliminación de comentarios.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -21,37 +21,31 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
 
 private slots:
-    // Cada uno reemplaza un "case" del switch original
+    
     void crearVector();
     void modificarVector();
     void eliminarVector();
     void calcularOperacion();
     void operacionSeleccionada(int index);
-    void cargarSeleccionEnCampos(int row); // al hacer click en la lista, carga X/Y/Z en los spinbox
+    void cargarSeleccionEnCampos(int row); 
 
 private:
     std::vector<Vector> listaVectores;
 
-    // Lista / panel izquierdo
     QListWidget *listaWidget;
 
-    // Plano cartesiano X-Z
     VectorCanvas *canvas;
-
-    // Vista 3D con OpenGL
     Vista3D *vista3d;
 
-    // Panel "crear / modificar"
+    
     QDoubleSpinBox *spinX, *spinY, *spinZ;
     QPushButton *btnCrear;
     QPushButton *btnModificar;
     QPushButton *btnEliminar;
-
-    // Panel "operar"
     QComboBox *comboOperacion;
     QComboBox *comboVector1;
-    QComboBox *comboVector2;     // se oculta si la operacion no necesita 2 vectores
-    QDoubleSpinBox *spinEscalar; // se muestra solo para mult. por escalar
+    QComboBox *comboVector2; 
+    QDoubleSpinBox *spinEscalar; 
     QCheckBox *checkGuardarResultado;
     QPushButton *btnCalcular;
     QLabel *labelResultado;
@@ -59,4 +53,4 @@ private:
     void refrescarLista();      // equivalente a mostrarVector()
     void refrescarCombosVectores();
 };
-#endif // MAINWINDOW_H
+#endif 
